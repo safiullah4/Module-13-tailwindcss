d7ea8f1b341f26aa3c70465e68fb8368224bc78b
MD Safiullah <77103203+safiullah4@users.noreply.github.com>
2025-01-25 03:29:36 +0000

    learned till video 8

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./index.html", // আপনার HTML ফাইলের পাথ
+          "./src/**/*.{html,js}", // আপনার src ফোল্ডারের সমস্ত HTML এবং JS ফাইল
+            ],
+              theme: {
+                  extend: {}, // আপনি কাস্টমাইজেশন এখানে যোগ করতে পারেন
+                    },
+                      plugins: [], // যদি কোনো Tailwind প্লাগিন ব্যবহার করেন
+                      }
+
+
+        //tailewind theke config code copy kore akhane boshaici. index.html jei folder e ace thik shei folder e index.html er pashe ata rakhte hobe tailwind extension install korar por
+        //aita maybe add hoy nai pc te korar shomoy check pls
+        //font add kori nai
